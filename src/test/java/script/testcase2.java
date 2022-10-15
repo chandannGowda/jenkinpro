c4c1c2c7a10c10e8fc2b4f4fb3ae61efc8de1d44
USER <USER@DESKTOP-RVOGI0F>
2022-10-15 15:01:34 +0530

    jenkins

---
+++ src/test/java/script/testcase2.java
@@ -0,0 +1,14 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testcase2 {
+	
+		@Test
+		public void tc2() {
+			Reporter.log("hello selenium",true);
+		}
+			
+
+}
---
+++ src/test/java/script/testcase4.java
@@ -0,0 +1,12 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testcase4 {
+		@Test
+		public void tc4() {
+			Reporter.log("hello api",true);
+			
+}
+}
